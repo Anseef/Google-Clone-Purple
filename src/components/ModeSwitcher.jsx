a12ada23468703a63465995004a653451f6cb546
Anseef <ansifpta0@gmail.com>
2023-11-09 21:09:49 +0530

    Implemented dark mode switcher.

--- src/components/ModeSwitcher.jsx
+++ src/components/ModeSwitcher.jsx
@@ -1,23 +1,37 @@
-import React, { useState } from 'react';
-import { RiToggleLine, RiToggleFill } from 'react-icons/ri';
+import React, { useState, useEffect } from 'react';
+import { MdOutlineDarkMode, MdLightMode } from 'react-icons/md';
 
 const ModeSwitcher = () => {
-  const [mode, setMode] = useState("dark");
-  const [ darkTheme, setDarkTheme ] = useState(false);
+  const storedMode = localStorage.getItem('mode') || "dark";
+  const [mode, setMode] = useState(storedMode);
+  const [darkTheme, setDarkTheme] = useState(storedMode === "dark");
+
+  const setTheme = (mode) => {
+    const root = document.documentElement;
+    if (mode === "dark") {
+      root.setAttribute("data-theme", "dark");
+    } else {
+      root.setAttribute("data-theme", "light");
+    }
+  };
+
+  useEffect(() => {
+    setTheme(mode);
+    localStorage.setItem('mode', mode);
+  }, [mode]);
+
   const handleModeToggle = () => {
     setDarkTheme(!darkTheme);
-    setMode((prevMode) => (prevMode === "dark" ? "light" : "dark"));
-    console.log(darkTheme);
+    setMode(darkTheme ? "light" : "dark");
   };
 
   return (
     <div className="modeContainer flex">
-      <span>Light</span>
       <button className="modeSwitcher grid" onClick={handleModeToggle}>
-        {mode === "dark" ? <RiToggleLine /> : <RiToggleFill />}
+        {mode === "light" ? <MdLightMode /> : <MdOutlineDarkMode />}
       </button>
-      <span>Dark</span>
     </div>
   );
 };
+
 export default ModeSwitcher;
