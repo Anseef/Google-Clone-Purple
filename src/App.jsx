69ede40ce0e93a9a742a742b4833b70ce06292c9
Anseef <ansifpta0@gmail.com>
2023-11-10 20:17:06 +0530

    Fix deployment with router 'basename' prop.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Home from './components/Home';
 import SearchMain from './components/SearchMain';
 
 function App() {
+  const pathPrefix = '/Google-Clone-Purple';
 
   return (
     <div className='App'>
-      <BrowserRouter>
+      <BrowserRouter basename={pathPrefix}>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route path='/search' element={<SearchMain/>}/>
+          <Route exact path="/" element={<Home />}/>
+          <Route path='/search' element={<SearchMain />}/>
         </Routes>
       </BrowserRouter>
     </div>
